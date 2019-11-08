da26ffdff4104a00ae565a6fe6461d855cded0cd
vbalogh <vbalogh@gpumaster01.lan.ultinous.com>
2019-11-08 08:23:09 +0000

    added detection printer runner

---
+++ run_eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film9 --outdir /data/vbalogh/MaskRCNN/detections/film9 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film8 --outdir /data/vbalogh/MaskRCNN/detections/film8 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film7 --outdir /data/vbalogh/MaskRCNN/detections/film7 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film6 --outdir /data/vbalogh/MaskRCNN/detections/film6 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film5 --outdir /data/vbalogh/MaskRCNN/detections/film5 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film4 --outdir /data/vbalogh/MaskRCNN/detections/film4 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film3 --outdir /data/vbalogh/MaskRCNN/detections/film3 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film2 --outdir /data/vbalogh/MaskRCNN/detections/film2 
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/film1 --outdir /data/vbalogh/MaskRCNN/detections/film1
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/HollywoodHeads/JPEGImages --outdir /data/vbalogh/MaskRCNN/detections/HollywoodHeads/JPEGImages
+python3 eval.py --images /data/vbalogh/head_det_corpus_v3/MPII/images --outdir /data/vbalogh/MaskRCNN/detections/MPII/images
