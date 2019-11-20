cd9d75ba5b3e29355806a2e81be774a73aee51f5
vbalogh-ultinous <vbalogh@ultinous.com>
2019-11-20 09:48:51 +0000

    updated aligner

--- aligner.sh
+++ aligner.sh
@@ -4,4 +4,6 @@ HEAD=${1-/home/vbalogh/personDet/head_det_v3_bounding_boxes/train_v3.csv}
 PERSON=${2-/data/vbalogh/yolact/detections/head_det_corpus_v3/film8}
 IMAGES=${3-/data/vbalogh/head_det_corpus_v3/film8}
 OUTDIR=${4-/data/vbalogh/yolact/align/film8}
-python align.py --head $HEAD --person $PERSON --images $IMAGES --outdir $OUTDIR
+NAME=${5-film8}
+METRICS=${6-metrics.json}
+python align.py --head $HEAD --person $PERSON --images $IMAGES --outdir $OUTDIR --name $NAME --metrics $METRICS
